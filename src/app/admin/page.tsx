6a0039cf9883abfa2bcd45d8b6ddbe2676b1b570
Md Mohaiminul Islam <mi@netzlab.de>
2024-06-02 17:37:53 +0200

    intiial

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminDashborad = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+        </div>
+    )
+}
+
+export default AdminDashborad
